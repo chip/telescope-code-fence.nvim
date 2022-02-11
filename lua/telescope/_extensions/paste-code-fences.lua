6b1d11c96c8a77658383cc1f16ac3fe91590372a
Chip Castle <chip@chipcastle.com>
2022-02-11 15:26:42 -0600

    Clean up plugin exports

--- lua/telescope/_extensions/paste-code-fences.lua
+++ lua/telescope/_extensions/paste-code-fences.lua
@@ -3,8 +3,5 @@ if not has_telescope then
   error("This plugin requires nvim-telescope/telescope.nvim")
 end
 
--- TODO local main = require('telescope._extensions.paste-code-fences.main')
-local main = require("telescope._extensions.paste-code-fences.find")
-return telescope.register_extension {
-  exports = {paste-code-fences = main, find = main.find}
-}
+local main = require("telescope._extensions.paste-code-fences.main")
+return telescope.register_extension {exports = {find = main.find}}
