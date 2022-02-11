24739cb28930dc70f2e8c2b9d7ea259a346a3948
Chip Castle <chip@chipcastle.com>
2022-02-11 14:55:15 -0600

    Remove unused files

--- lua/telescope/_extensions/chunky-code/utils.lua
+++
@@ -1,6 +0,0 @@
--- Source: https://github.com/alpha2phi/alpha.nvim/blob/main/lua/alpha/init.lua
--- Debugging dump
-function _G.dump(...)
-  local objects = vim.tbl_map(vim.inspect, {...})
-  print(unpack(objects))
-end
